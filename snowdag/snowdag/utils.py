45b2e0831fb216aed1b04a5aecbf537c8e65b396
Nick Pelikan <nick.pelikan@posit.co>
2025-02-10 09:37:19 -0700

    dgdfsdg

--- snowdag/snowdag/utils.py
+++ snowdag/snowdag/utils.py
@@ -1,7 +1,15 @@
 def get_s3_objects(s3_client, s3_bucket: str, s3_prefix: str) -> list[str]:
-    _truncated = True
+
     keys = []
-    continuation_token = ""
+
+    objects = s3_client.list_objects_v2(
+        Bucket=s3_bucket,
+        MaxKeys=1000,
+        Prefix=s3_prefix,
+    )
+    keys = keys + objects["Contents"]
+    _truncated = objects["IsTruncated"]
+    continuation_token = objects["NextContinuationToken"]
 
     while _truncated:
         objects = s3_client.list_objects_v2(
